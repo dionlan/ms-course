9ff177613c64244c71a17cf6e620ca6b80fb3d84
dionlan <dionlan.alves@gmail.com>
2022-01-28 14:49:52 -0300

    User, Role, @ManyToMany association.

---
+++ hr-user/src/main/java/com/dionlan/hruser/entities/Role.java
@@ -0,0 +1,29 @@
+package com.dionlan.hruser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "tb_role")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "role_name")
+	private String roleName;
+}
---
+++ hr-user/src/main/java/com/dionlan/hruser/entities/User.java
@@ -0,0 +1,42 @@
+package com.dionlan.hruser.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+		joinColumns = @JoinColumn(name = "user_id"), 
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+}
